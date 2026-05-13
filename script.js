cf798bd6ea9b6255c0c0b276b28d7e51d9860f16
Kaviya Rajendran <149192857+KaviyaRajendran031022@users.noreply.github.com>
2026-05-13 06:09:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("Modern Portfolio Loaded");
+
+/* TYPING EFFECT */
+
+new Typed(".typing", {
+
+    strings: [
+
+        "Full Stack Developer",
+        "Python Developer",
+        "Java Enthusiast",
+        "Frontend Designer",
+        "AI Developer"
+
+    ],
+
+    typeSpeed: 70,
+    backSpeed: 50,
+    loop: true
+
+});
+
+/* AOS */
+
+AOS.init({
+
+    duration:1000,
+    once:true
+
+});
